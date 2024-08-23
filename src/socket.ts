10c93c73441057d6718a6e1d9bf85403d48ff036
Martin Ibarra <8990184+MartinIbarra@users.noreply.github.com>
2024-08-23 09:27:31 -0300

    vite env variable

--- src/socket.ts
+++ src/socket.ts
@@ -1,10 +1,11 @@
 import { io, Socket } from "socket.io-client";
+// import process from "process";
 import {
   ServerToClientEvents,
   ClientToServerEvents,
 } from "./types/socket.types";
 
-const URL: string = process.env.BACKEND_ENDPOINT as string;
+const URL: string = import.meta.env.VITE_BACKEND_ENDPOINT as string;
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   URL,
